5b0a37f0f8009ea42d72e3429c69bf6b7f68c283
Shivani Vohra Gandhi <shivanivohragandhi@gmail.com>
2021-05-24 02:08:38 +0530

    profile tab updated

---
+++ Alysei/Controller/ProfileModule/ProfileViewContainer/TabCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TabCollectionViewCell.swift
+//  Alysei
+//
+//  Created by Janu Gandhi on 23/05/21.
+//
+
+import UIKit
+
+class TabCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+
+}
+
+
---
+++ Alysei/Model/ProfileTabModels/ProfileTabRows.swift
@@ -0,0 +1,78 @@
+//
+//  ProfileTabRows.swift
+//  Alysei
+//
+//  Created by Janu Gandhi on 23/05/21.
+//
+
+import Foundation
+
+struct ProfileTabRows {
+
+    func noOfRows(_ userRole: UserRoles) -> Int {
+//        switch userRole {
+//        case .producer:
+//            return 5
+//        case .distributer1:
+//            return 5
+//        case .distributer2:
+//            return 5
+//        case .distributer3:
+//            return 5
+//        case .voiceExperts:
+//            return 5
+//        case .travelAgencies:
+//            return 5
+//        case .restaurant:
+//            return 5
+//        case .voyagers:
+//            return 5
+//        }
+
+        return self.rowsTitle(userRole).count
+    }
+
+    func rowsTitle(_ userRole: UserRoles) -> [String] {
+        switch userRole {
+        case .producer:
+            return ["Post" , "Photos", "About", "Contact", "Awards"]
+        case .distributer1:
+            return ["Post" , "Photos", "About", "Contact", "Awards"]
+        case .distributer2:
+            return ["Post" , "Photos", "About", "Contact", "Awards"]
+        case .distributer3:
+            return ["Post" , "Photos", "About", "Contact", "Awards"]
+        case .voiceExperts:
+            return ["Post" , "Photos", "About", "Contact", "Blogs"]
+        case .travelAgencies:
+            return ["Post" , "Photos", "About", "Contact", "Awards"]
+        case .restaurant:
+            return ["Post" , "Photos", "About", "Contact", "Awards"]
+        case .voyagers:
+            return ["Post" , "Photos", "About", "Contact", "Awards"]
+        }
+    }
+
+
+    func imageName(_ userRole: UserRoles) -> [String] {
+        switch userRole {
+        case .producer:
+            return ["posts_icon_normal", "posts_icon_normal", "about_icon_normal", "contact_icon_normal", "contact_icon_normal" ]
+        case .distributer1:
+            return ["posts_icon_normal", "posts_icon_normal", "about_icon_normal", "contact_icon_normal", "contact_icon_normal" ]
+        case .distributer2:
+            return ["posts_icon_normal", "posts_icon_normal", "about_icon_normal", "contact_icon_normal", "contact_icon_normal" ]
+        case .distributer3:
+            return ["posts_icon_normal", "posts_icon_normal", "about_icon_normal", "contact_icon_normal", "contact_icon_normal" ]
+        case .voiceExperts:
+            return ["posts_icon_normal", "posts_icon_normal", "about_icon_normal", "contact_icon_normal", "contact_icon_normal" ]
+        case .travelAgencies:
+            return ["posts_icon_normal", "posts_icon_normal", "about_icon_normal", "contact_icon_normal", "contact_icon_normal" ]
+        case .restaurant:
+            return ["posts_icon_normal", "posts_icon_normal", "about_icon_normal", "contact_icon_normal", "contact_icon_normal" ]
+        case .voyagers:
+            return ["posts_icon_normal", "posts_icon_normal", "about_icon_normal", "contact_icon_normal", "contact_icon_normal" ]
+        }
+    }
+
+}
